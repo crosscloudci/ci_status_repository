365fa67f0286f439a88b3ad768a7474ba28fcb34
wolfpack <wolfpack@vulk.co>
2017-10-23 18:15:47 -0400

    gitlab yml reader now uses an env variable

--- lib/yml_reader/gitlab_ci.ex
+++ lib/yml_reader/gitlab_ci.ex
@@ -3,13 +3,12 @@ defmodule CncfDashboardApi.YmlReader.GitlabCi do
 	def get do
 		Application.ensure_all_started :inets
 
-    {:ok, resp} = :httpc.request(:get, {'https://gitlab.cncf.ci/cncf/cross-cloud/raw/ci-stable-v0.1.0/.gitlab-ci.yml', []}, [], [body_format: :binary])
+    {:ok, resp} = :httpc.request(:get, {System.get_env("GITLAB_CI_YML") |> to_charlist, []}, [], [body_format: :binary])
 		{{_, 200, 'OK'}, _headers, body} = resp
     body
   end
   def cloud_list do
     yml = CncfDashboardApi.YmlReader.GitlabCi.get() |> YamlElixir.read_from_string 
-    IEx.pry
     yml["variables"]["ACTIVE_CLOUDS"]
   end
 end
