8a0b35ff2c9ab70419372bd028a116f78cbecc22
wolfpack <wolfpack@vulk.co>
2017-11-09 18:23:13 -0500

    removed comment from factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -41,25 +41,4 @@ defmodule CncfDashboardApi.Factory do
     }
 	end
 
-  # def user_factory do
-  #   %MyApp.User{
-  #     name: "Jane Smith",
-  #     email: sequence(:email, &"email-#{&1}@example.com"),
-  #   }
-  # end
-  #
-  # def article_factory do
-  #   %MyApp.Article{
-  #     title: "Use ExMachina!",
-  #     # associations are inserted when you call `insert`
-  #     author: build(:user),
-  #   }
-  # end
-  #
-  # def comment_factory do
-  #   %MyApp.Comment{
-  #     text: "It's great!",
-  #     article: build(:article),
-  #   }
-  # end
 end
