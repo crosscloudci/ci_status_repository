e60a577888a07deb500aca85d6a7f4dc62d89264
wolfpack <wolfpack@vulk.co>
2017-11-14 23:32:30 -0500

    gitlab monitor now sends a dashboard channel message

--- lib/gitlab_monitor/gitlab_monitor.ex
+++ lib/gitlab_monitor/gitlab_monitor.ex
@@ -5,40 +5,41 @@ defmodule CncfDashboardApi.GitlabMonitor do
   def upsert_pipeline_monitor(source_key_project_monitor_id) do
     monitor = CncfDashboardApi.Repo.all(from skpm in CncfDashboardApi.SourceKeyProjectMonitor, 
                                         where: skpm.id == ^source_key_project_monitor_id) |> List.first
-    # field :source_project_id, :string
-    # field :source_pipeline_id, :string
-    # field :source_pipeline_job_id, :string
-    # field :pipeline_release_type, :string
-    # field :active, :boolean, default: true
     # migrate project
     {:ok, upsert_count, project_map} = CncfDashboardApi.GitlabMigrations.upsert_project(monitor.source_project_id) 
 
-    # TODO get the local project id
+    # get the local project id
     source_key_project = CncfDashboardApi.Repo.all(from skp in CncfDashboardApi.SourceKeyProjects, 
                                                    where: skp.source_id == ^monitor.source_project_id) |> List.first
 
-    # TODO call pipeline Data migration
+    # migrate pipeline 
     {:ok, upsert_count, pipeline_map} = CncfDashboardApi.GitlabMigrations.upsert_pipeline(monitor.source_project_id, monitor.source_pipeline_id) 
-    # TODO get the local pipeline
+
+    # get the local pipeline
     source_key_pipeline = CncfDashboardApi.Repo.all(from skp in CncfDashboardApi.SourceKeyPipelines, 
                                                    where: skp.source_id == ^monitor.source_pipeline_id) |> List.first
-    # # TODO set the project id in the pipeline monitor source of truth
+
+    
+    # determine pipeline type
+    case CncfDashboardApi.GitlabMonitor.is_deploy_pipeline_type(source_key_project.new_id) do
+      true -> pipeline_type = "deploy"
+      _ -> pipeline_type = "build"
+    end
+
+    # saved curl data (source key project monitor)                                                
     # field :source_project_id, :string
     # field :source_pipeline_id, :string
     # field :source_pipeline_job_id, :string
     # field :pipeline_release_type, :string
     # field :active, :boolean, default: true
-    # 
+    
+    # pipeline monitor fields
     # field :pipeline_id, :integer
     # field :running, :boolean, default: false
     # field :release_type, :string
     # field :pipeline_type, :string
     # field :project_id, :integer
-    # pipeline_type = if (CncfDashboardApi.GitlabMonitor.is_deploy_pipeline_type(source_key_project.new_id)), do: "deploy", else: "build"
-    case CncfDashboardApi.GitlabMonitor.is_deploy_pipeline_type(source_key_project.new_id) do
-      true -> pipeline_type = "deploy"
-      _ -> pipeline_type = "build"
-    end
+    
     changeset = CncfDashboardApi.PipelineMonitor.changeset(%CncfDashboardApi.PipelineMonitor{}, 
                                                            %{project_id: source_key_project.new_id,
                                                              pipeline_id: source_key_pipeline.new_id,
@@ -52,33 +53,31 @@ defmodule CncfDashboardApi.GitlabMonitor do
 
     # TODO start polling
      
-    # TODO determine pipeline type
+    # Call dashboard channel
     
-    # TODO set the pipeline id, running = true, release_type, pipeline_type  in the pipeline monitor source of truth
-    
-    # TODO call dashboard channel
+    CncfDashboardApi.Endpoint.broadcast! "dashboard:*", "new_cross_cloud_call", %{reply: dashboard_response} 
     
     # TODO update last updated
     
-    # case Repo.insert(changeset) do
-    #   {:ok, source_key_project_monitor} ->
-    #     conn
-    #     |> put_status(:created)
-    #     |> put_resp_header("location", source_key_project_monitor_path(conn, :show, source_key_project_monitor))
-    #     |> render("show.json", source_key_project_monitor: source_key_project_monitor)
-    #   {:error, changeset} ->
-    #     conn
-    #     |> put_status(:unprocessable_entity)
-    #     |> render(CncfDashboardApi.ChangesetView, "error.json", changeset: changeset)
-    # end
-    # put local project id in pipeline_monitor
-    #
-    # field :pipeline_id, :integer
-    # field :running, :boolean, default: false
-    # field :release_type, :string
-    # field :pipeline_type, :string
-    # field :project_id, :integer
-    # call monitor pipleine
+  end
+
+  def dashboard_response do
+    cloud_list = CncfDashboardApi.Repo.all(from cd1 in CncfDashboardApi.Clouds, 
+                                           where: cd1.active == true,
+                                           select: %{id: cd1.id, cloud_id: cd1.id, 
+                                             name: cd1.cloud_name, cloud_name: cd1.cloud_name}) 
+    projects = CncfDashboardApi.Repo.all(from projects in CncfDashboardApi.Projects,      
+                                         left_join: pipelines in assoc(projects, :pipelines),
+                                         left_join: pipeline_jobs in assoc(pipelines, :pipeline_jobs),
+                                         left_join: cloud in assoc(pipeline_jobs, :cloud),
+                                         where: projects.active == true,
+                                         preload: [pipelines: 
+                                                   {pipelines, pipeline_jobs: pipeline_jobs, 
+                                                     pipeline_jobs: {pipeline_jobs, cloud: cloud },
+                                                   }] )
+
+    with_cloud = %{"clouds" => cloud_list, "projects" => projects} 
+    response = CncfDashboardApi.DashboardView.render("index.json", dashboard: with_cloud)
   end
 
   def is_deploy_pipeline_type(project_id) do
--- test/lib/gitlab_monitor_test.exs
+++ test/lib/gitlab_monitor_test.exs
@@ -2,21 +2,30 @@ require IEx;
 # require CncfDashboardApi.DataMigrations;
 require Logger;
 defmodule CncfDashboardApi.GitlabMonitorTest do
+  use CncfDashboardApi.ChannelCase
+
+  alias CncfDashboardApi.DashboardChannel
+
   import Ecto.Query
   import CncfDashboardApi.Factory
   # use EctoConditionals, repo: CncfDashboardApi.Repo
   use ExUnit.Case
-  use CncfDashboardApi.ModelCase
-
-
+  # use CncfDashboardApi.ModelCase
+  
   @tag :wip
+  # test "upsert_pipeline_monitor", %{socket: socket} do 
   test "upsert_pipeline_monitor" do 
     skpm = insert(:source_key_project_monitor)
     # check insert 
+    CncfDashboardApi.Endpoint.subscribe(self, "dashboard:*")
+    {:ok, upsert_count, cloud_map} = CncfDashboardApi.GitlabMigrations.upsert_clouds()
+    projects = insert(:project)
     CncfDashboardApi.GitlabMonitor.upsert_pipeline_monitor(skpm.id)
     pipeline_monitor_count = CncfDashboardApi.Repo.aggregate(CncfDashboardApi.PipelineMonitor, :count, :id)  
     # source_project_count = CncfDashboardApi.Repo.aggregate(CncfDashboardApi.SourceKeyProjects, :count, :id)  
     assert 1 = pipeline_monitor_count  
+    assert_receive %Phoenix.Socket.Broadcast{ topic: "dashboard:*", 
+      event: "new_cross_cloud_call", payload: %{reply: %{dashboard: dashboard}}}
     # assert 1 < project_count  
     # assert 1 < source_project_count
     # # check update -- should not increase
@@ -24,7 +33,7 @@ defmodule CncfDashboardApi.GitlabMonitorTest do
     # assert project_count = CncfDashboardApi.Repo.aggregate(CncfDashboardApi.Projects, :count, :id)  
     # assert source_project_count = CncfDashboardApi.Repo.aggregate(CncfDashboardApi.SourceKeyProjects, :count, :id)  
   end
-  @tag :wip
+
   test "is_deploy_pipeline_type" do 
     project = insert(:project)
     assert CncfDashboardApi.GitlabMonitor.is_deploy_pipeline_type(project.id) == false
