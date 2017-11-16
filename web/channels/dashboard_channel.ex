a05f66c63df04573a2abeb3eb2bfcfde7c853d5c
wolfpack <wolfpack@vulk.co>
2017-11-15 22:08:22 -0500

    ref_monitor now in the dashboard channel

--- web/channels/dashboard_channel.ex
+++ web/channels/dashboard_channel.ex
@@ -8,14 +8,23 @@ defmodule CncfDashboardApi.DashboardChannel do
                                            where: cd1.active == true,
                                            select: %{id: cd1.id, cloud_id: cd1.id, 
                                              name: cd1.cloud_name, cloud_name: cd1.cloud_name}) 
+    # projects = CncfDashboardApi.Repo.all(from projects in CncfDashboardApi.Projects,      
+    #                                      left_join: pipelines in assoc(projects, :pipelines),
+    #                                      left_join: pipeline_jobs in assoc(pipelines, :pipeline_jobs),
+    #                                      left_join: cloud in assoc(pipeline_jobs, :cloud),
+    #                                      where: projects.active == true,
+    #                                      preload: [pipelines: 
+    #                                                {pipelines, pipeline_jobs: pipeline_jobs, 
+    #                                                  pipeline_jobs: {pipeline_jobs, cloud: cloud },
+    #                                                }] )
     projects = CncfDashboardApi.Repo.all(from projects in CncfDashboardApi.Projects,      
-                                         left_join: pipelines in assoc(projects, :pipelines),
-                                         left_join: pipeline_jobs in assoc(pipelines, :pipeline_jobs),
-                                         left_join: cloud in assoc(pipeline_jobs, :cloud),
+                                         left_join: ref_monitors in assoc(projects, :ref_monitors),
+                                         left_join: dashboard_badge_statuses in assoc(ref_monitors, :dashboard_badge_statuses),
+                                         left_join: cloud in assoc(dashboard_badge_statuses, :cloud),
                                          where: projects.active == true,
-                                         preload: [pipelines: 
-                                                   {pipelines, pipeline_jobs: pipeline_jobs, 
-                                                     pipeline_jobs: {pipeline_jobs, cloud: cloud },
+                                         preload: [ref_monitors: 
+                                                   {ref_monitors, dashboard_badge_statuses: dashboard_badge_statuses, 
+                                                     dashboard_badge_statuses: {dashboard_badge_statuses, cloud: cloud },
                                                    }] )
 
     with_cloud = %{"clouds" => cloud_list, "projects" => projects} 
