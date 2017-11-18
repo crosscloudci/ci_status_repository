9d17f56ee0abbb1d6ff2999bfc278358a35965ef
Taylor Carpenter <taylor@vulk.coop>
2017-11-18 01:42:35 -0500

    split create and migrate

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,8 @@ done
 sleep 5
 echo "Connected!"
 
-mix ecto.create && mix ecto.migrate
+mix ecto.create || true
+mix ecto.migrate
 mix gitlab_data.load_clouds
 mix gitlab_data.load_projects
 mix phoenix.server
